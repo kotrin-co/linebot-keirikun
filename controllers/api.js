52eb4825a4fd599725d5fa905494405d2ca17310
kotrin-co <46315612+kotrin-co@users.noreply.github.com>
2021-01-11 18:16:31 +0900

    api

--- controllers/api.js
+++ controllers/api.js
@@ -5,7 +5,7 @@ module.exports = {
   getUser: (req,res) => {
     const line_uid = req.query.line_uid;
     User.getUserData(line_uid)
-      then(data=>{
+      .then(data=>{
         res.status(200).json(data);
       })
       .catch(e=>console.log(e));
