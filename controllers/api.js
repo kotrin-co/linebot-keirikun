1d3e2febcdde901ab4c94a4bb1ebfc97e8a31e5d
kotrin-co <46315612+kotrin-co@users.noreply.github.com>
2021-01-11 20:22:55 +0900

    post data

--- controllers/api.js
+++ controllers/api.js
@@ -1,14 +1,19 @@
-const User = require('../models/User');
+const Data = require('../models/Data');
 
 module.exports = {
 
   getUser: (req,res) => {
     const line_uid = req.query.line_uid;
-    User.getUserData(line_uid)
+    Data.getUserData(line_uid)
       .then(data=>{
         console.log('data in controller',data);
         res.status(200).json(data);
       })
       .catch(e=>console.log(e));
+  },
+
+  postData: (req,res) => {
+    console.log('req.body',req.body);
+    const data = req.body;
   }
 }
--- routers/api.js
+++ routers/api.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const controller = require('../controllers/api');
 
 router
-    .route('/')
-    .get(controller.getUser);
+  .route('/')
+  .get(controller.getUser);
+
+router
+  .route('/')
+  .post(controller.postData);
 
 module.exports = router;
