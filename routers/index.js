bb6c027125d7f0ab8ed5c75b3bf9699d0609a2f1
kotrin-co <46315612+kotrin-co@users.noreply.github.com>
2021-01-16 19:01:24 +0900

    settlement

--- routers/index.js
+++ routers/index.js
@@ -5,6 +5,11 @@ const router = express.Router();
 router 
     .get('/',(req,res)=>{
         res.render('pages/index');
+    });
+
+router
+    .get('/settlement',(req,res)=>{
+        res.render('pages/settlement')
     })
 
 module.exports = router;
