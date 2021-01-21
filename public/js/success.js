a9a2ea02a2a4206a67d2e5b09f249090924789d0
kotrin-co <46315612+kotrin-co@users.noreply.github.com>
2021-01-21 19:55:55 +0900

    manage billing form

--- public/js/success.js
+++ public/js/success.js
@@ -27,36 +27,46 @@ window.onload = () => {
               .then((result)=>{
                 return result.json();
               })
-              .then((session)=>{
+              .then(session=>{
                 // customerId = session.customer;
                 const sessionJSON = JSON.stringify(session,null,2);
-                document.querySelector('pre').textContent = sessionJSON;
+                const pre = document.createElement('pre');
+                pre.textContent = sessionJSON;
                 subscription = session.subscription;
+
+                const manageBillingForm = document.createElement('form');
+                const submitButton = document.createElement('button');
+                manageBillingForm.appendChild(submitButton);
+                // const manageBillingForm = document.querySelector('#manage-billing-form');
+                manageBillingForm.addEventListener('submit',(e)=>{
+                  e.preventDefault();
+                  fetch('/customer-portal',{
+                    method: 'POST',
+                    headers: {
+                      'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                      sessionId: sessionId
+                    })
+                  })
+                  .then(response=>response.json())
+                  .then(data=>{
+                    liff.openWindow({
+                      url: data.url,
+                      external: false
+                    });
+                    // window.location.href=data.url;
+                  })
+                  .catch(error=>{
+                    console.error('error:',error);
+                  });
+                });
+                divPage.appendChild(manageBillingForm);
+                divPage.appendChild(pre);
               })
               .catch(err=>{
                 console.log('Error when fetching Checkout session', err);
               });
-          
-              // const manageBillingForm = document.querySelector('#manage-billing-form');
-              // manageBillingForm.addEventListener('submit',(e)=>{
-              //   e.preventDefault();
-              //   fetch('/customer-portal',{
-              //     method: 'POST',
-              //     headers: {
-              //       'Content-Type': 'application/json'
-              //     },
-              //     body: JSON.stringify({
-              //       sessionId: sessionId
-              //     })
-              //   })
-              //   .then(response=>response.json())
-              //   .then(data=>{
-              //     window.location.href=data.url;
-              //   })
-              //   .catch(error=>{
-              //     console.error('error:',error);
-              //   });
-              // });
           
               // const cancelButton = document.getElementById('cancel-button').addEventListener('click',(e)=>{
               //   e.preventDefault();
