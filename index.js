e71805a27409259284d09af5ac4377241a8263a5
kotrin-co <46315612+kotrin-co@users.noreply.github.com>
2021-01-22 22:16:36 +0900

    settle back

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const multipart = require('connect-multiparty');
 const privatekey = require('./client_secret.json');
 const router = require('./routers/index');
 const apiRouter = require('./routers/api');
-const checkoutRouter = require('./routers/payment');
+const settlementRouter = require('./routers/settlement');
 const Data = require('./models/Data');
   const original_SSID = '13Y2AZYNHWnQNKdSzK5Vxna_YPdf4YnT61imptdiM_MU';
   const original_SID = [0,1686142823];
@@ -63,7 +63,9 @@ express()
       }
     })
   )
-  .use('/checkout',checkoutRouter)
+  // .get('/settlement',settlementRouter)
+  .get('/success',settlementRouter)
+  // .get('/canceled',settlementRouter)
   .get('/checkout-session',async (req,res)=>{
     const { sessionId } = req.query;
     const session = await stripe.checkout.sessions.retrieve(sessionId);
--- routers/payment.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/payment');
-
-router
-  .route('/success')
-  .get(controller.displaySuccess);
-
-module.exports = router;
