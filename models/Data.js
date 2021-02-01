901e0fe377acdefc2cc34b2f6f63c2f7112356bb
kotrin-co <46315612+kotrin-co@users.noreply.github.com>
2021-02-01 20:23:39 +0900

    modified

--- models/Data.js
+++ models/Data.js
@@ -95,6 +95,8 @@ const updateJournal = (ssId) => {
                 let month = 1;
                 //ACCOUNTSのインデックス計算　雑収入のみ２項目のため、そのための処理を入れる
                 const accountNumber = j<=82 ? Math.floor(j/3) : Math.round(j/3);
+                //源泉所得税の場合だけ摘要に特別処理を行う
+                const description = j === 2 ? '源泉所得税' : ACCOUNTS[j];
                 while(days>daysEveryMonth[month-1]){
                   days -= daysEveryMonth[month-1];
                   month++;
@@ -105,7 +107,7 @@ const updateJournal = (ssId) => {
                   value,
                   CREDITS[j],
                   value,
-                  ACCOUNTS[accountNumber]
+                  description
                 ]);
               }
             })
