36ba517120e27c81854f7bf120669fac1fb5ec64
kotrin-co <46315612+kotrin-co@users.noreply.github.com>
2021-03-13 08:33:14 +0900

    day test

--- index.js
+++ index.js
@@ -212,7 +212,7 @@ const handleMessageEvent = async (ev) => {
             else if(text === 'テスト'){
               console.log('today',new Date().getTime());
               const day = new Date().getDate();
-              const day2 = new Date(new Date().getTime()).getDate();
+              const day2 = new Date(new Date().getTime()+9*60*60*1000).getDate();
               return client.replyMessage(ev.replyToken,{
                 type: 'text',
                 text: `day1 ${day} day2 ${day2}`
